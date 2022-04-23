454ea6693b1a48f874e7c14b37da568255f00aba
bhargavraju49 <58097588+bhargavraju49@users.noreply.github.com>
2022-04-23 10:38:21 +0530

    Time: 89 ms (61.21%), Space: 14.9 MB (95.64%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,23 +1,20 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        k=[]
-        for i in nums:
-            k.append(i)
-        print(k)
-        s=0
-        e=len(nums)-1
-        nums.sort()
-        for i in range(len(nums)):
-            if (nums[s]+nums[e]==target):
-                if nums[e]!=nums[s]:
-                    return [k.index(nums[s]),k.index(nums[e])]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        k = [i for i in nums]
+        nums.sort();
+        start = 0
+        end = len(nums) - 1
+        
+        while start < end:
+            if nums[start]+nums[end] == target:
+                if nums[end]!=nums[start]:
+                    return [k.index(nums[start]),k.index(nums[end])]
                 else:
-                    a=k.index(nums[s])
+                    a=k.index(nums[start])
                     k[a]='x'
-                    b=k.index(nums[s])
+                    b=k.index(nums[start])
                     return(a,b)
-            elif (nums[s]+nums[e]>target):
-                e=e-1
-            else:
-                s=s+1
-                
+            elif nums[start]+nums[end] > target:
+                end-=1
+            elif nums[start]+nums[end] < target:
+                start+=1

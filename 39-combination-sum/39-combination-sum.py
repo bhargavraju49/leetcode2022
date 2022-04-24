81c033c9a963f3edc3380ddd3a5bdc9a5ef980d0
bhargavraju49 <58097588+bhargavraju49@users.noreply.github.com>
2022-04-24 22:48:45 +0530

    Time: 75 ms (85.70%), Space: 13.9 MB (92.19%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,24 +2,24 @@ class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         
-        
+        #sorting before calling
         
         def result(nums , target):
-            res = []
+            res = []                                        
             for i in nums:
-                if i == target:
-                    res.append([i])
+                if i == target:         # every entry go from here if fails [] returns always such that entry wont takes place 
+                    res.append([i])    #storing solo grouping and return after first call to add them with another new target
             
             for i in range(len(nums)):
                 v = nums[i]
                 
-                if v>target//2:
+                if v>target//2:         # at this point no use of checking further as all are greater than what we need
                     break
                     
                 else:
-                    res2 = result(nums[i:],target-v)
+                    res2 = result(nums[i:],target-v)  # calling with same index ensures taking repetetion in call stacks
                     for k in res2:
-                        res.append([v] + k)
+                        res.append([v] + k)  # adding them in array format 
             
             return res
         

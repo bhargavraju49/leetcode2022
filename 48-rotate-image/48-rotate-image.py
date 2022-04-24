9ac62b8a0db99c2da0e8e8fc0a6f9624a65b430b
bhargavraju49 <58097588+bhargavraju49@users.noreply.github.com>
2022-04-24 23:24:45 +0530

    Time: 60 ms (26.06%), Space: 13.8 MB (77.08%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -5,7 +5,7 @@ class Solution:
         
         n = len(matrix)
         
-        for i in range(0,n):
+        for i in range(n):
             for j in range(i+1,n):
                 matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
             matrix[i] = matrix[i][::-1]

05fd3d55c1d5d8b17436e52bc2122958e3743ad7
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-05-10 23:26:59 +0530

    Time: 44 ms (51.32%), Space: 13.9 MB (79.39%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,21 +1,21 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        mi = (k * (k + 1)) // 2
-        ma = 45 - ((9 - k) * (10 - k)) // 2
-        if n == mi:
-            res = []
-            for i in range(1, k + 1):
-                res.append(i)
-            return [res]
+#         mi = (k * (k + 1)) // 2
+#         ma = 45 - ((9 - k) * (10 - k)) // 2
+#         if n == mi:
+#             res = []
+#             for i in range(1, k + 1):
+#                 res.append(i)
+#             return [res]
 
-        elif n == ma:
-            res = []
-            for i in range(9 - k + 1, 10):
-                res.append(i)
-            return [res]
+#         elif n == ma:
+#             res = []
+#             for i in range(9 - k + 1, 10):
+#                 res.append(i)
+#             return [res]
 
-        elif n > ma or n < mi:
-            return []
+#         elif n > ma or n < mi:
+#             return []
 
         def combi (k,n,s,l):
             if k == 1 and n>=s and n<10:

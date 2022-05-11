be73b83ce1709fd2530d2f8b0e091b74189c4c67
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-05-11 09:25:38 +0530

    Time: 55 ms (32.02%), Space: 13.8 MB (65.31%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -6,8 +6,8 @@ class Solution:
             k = [sigN]
             for j in range(1,5):
                 k.append(k[j-1] - N[j-1])
+                sigN+=k[j]
             N=k
-            sigN = sum(N)
             pass
         return (sigN)
             

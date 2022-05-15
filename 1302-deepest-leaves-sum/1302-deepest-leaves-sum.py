9af9bb06fee68631ac8189492f5dc657fc7ff3d5
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-05-16 00:34:40 +0530

    Time: 383 ms (18.63%), Space: 17.8 MB (66.46%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -6,50 +6,31 @@
 #         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        
-        #####
-        #DFS#
-        #####
-        
-        
-#         def helper(root):
-#             if  root is None:
-#                 return 0
-#             l = 1 + helper(root.left)
-#             r = 1 + helper(root.right)
-#             return max(l,r)
-#         height = helper(root)
-        
-#         def counter(root,h):
-#             if  root is None:
-#                 return 0
-#             l = counter(root.left,h-1)
-#             r = counter(root.right,h-1)
-#             if h==1:
-#                 return root.val
-#             return l+r
-        
-#         k = counter(root,height)
-#         return k
-        
-        #####
-        #BFS#
-        #####
-        
         q = [root]
-        count = 0
+        sum1 = root.val
+        
         while q:
-            it = len(q)
-            summ = 0
-            for i in range(it):
+            l = len(q)
+            tempsum = 0
+            
+            for i in range(l):
                 node = q.pop(0)
-                summ+=node.val
                 if node.left:
                     q.append(node.left)
+                    tempsum+=node.left.val
                 if node.right:
                     q.append(node.right)
-            if summ:
-                count=summ
-        return count
+                    tempsum+=node.right.val
+            if tempsum:
+                sum1 = tempsum
+        
+        return sum1
+
+                
+                
+            
+            
+                    
+        
             
                 

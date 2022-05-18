86e9a4705a20a67bf94277f25634bb6b2f08874d
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-05-18 18:17:09 +0530

    Time: 3470 ms (34.99%), Space: 72 MB (83.72%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -5,31 +5,22 @@ class Solution:
         for i,j in connections:
             graph[i].append(j)
             graph[j].append(i)
-            
-        
         time = [None]*n
         res = []
-        
         def getcc(node = 0, parent = -1, t = 1):
             if time[node]:
                 return
-            
             time[node] = t
-            
             for Nvertex in graph[node]:
                 if Nvertex == parent:
                     continue
-                
                 getcc(Nvertex, node, t+1)
-                
                 if time[Nvertex] == t+1:
                     res.append((node,Nvertex))
                 else:
                     time[node] = min(time[Nvertex],time[node]) 
-            
-            return res
-        
-        return getcc()
+        getcc()
+        return res
                     
                 
                 

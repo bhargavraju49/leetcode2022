eaa22665d354d65a44d245b6a22cfaff3b89c0ba
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-05-19 15:49:04 +0530

    Time: 325 ms (45.11%), Space: 25.4 MB (77.41%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,11 +1,9 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        N = len(nums)
-        if k>N:
-            k=k%N
+        if k>len(nums):
+            k=k%len(nums)
         if len(nums)!=1:
-            p=nums[-k:]+nums[:-k]
-            for i in range(len(nums)):
-                nums[i]=p[i]
+            nums[:]=nums[-k:]+nums[:-k]
+
 
         

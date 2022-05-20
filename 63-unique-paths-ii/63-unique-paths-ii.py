e03caf510bc3fb83891ba3abb825619cd394129d
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-05-20 08:21:32 +0530

    Time: 59 ms (47.15%), Space: 13.9 MB (74.88%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        if grid[0][0]==1 or grid[-1][-1]==1:
+            return 0
         m,n = len(grid),len(grid[0])
         dp = [0 for i in range(n+1)]
         dp[1] = 1

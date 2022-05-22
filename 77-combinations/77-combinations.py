b7e2107dfcc3cd980242396e7a17068e0a601324
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-05-22 22:36:47 +0530

    Time: 718 ms (20.38%), Space: 16.1 MB (19.69%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res =[]
+        def helper(n,k,r = []):
+            if k == 0:
+                res.append(r)
+            
+            for i in range(len(n)):
+                helper(n[i+1:],k-1,r+[n[i]])
+        
+        helper([i for i in range(1,n+1)],k)
+        return res

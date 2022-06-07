66abcf040c4aa626497f247a7b908aa7649fcd3b
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-06-07 08:36:36 +0530

    Time: 47 ms (61.54%), Space: 14 MB (37.34%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,20 +1,16 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-
-
-        p1, p2 = m-1, n-1
-
-        for d in range(m+n-1, -1, -1):
-
-            if p2 < 0:                              # nums 2 empty nothing to do
-                return
-
-            if p1 >= 0 and nums1[p1] > nums2[p2]:    # cloning the big number to right zeros            
-                nums1[d] = nums1[p1]
-                p1 -= 1
+        while m>0 and n>0:
+            
+            if nums2[n-1] >= nums1[m-1]:
+                nums1[m+n-1] = nums2[n-1]
+                n-=1
+            else:
+                nums1[m+n-1] = nums1[m-1]
+                m-=1
                 
                 
-            else:                                    # updating the small element with index help from d 
-                nums1[d] = nums2[p2]
-                p2 -= 1
+        while n>0:
+            nums1[m+n-1] = nums2[n-1]
+            n-=1
                       

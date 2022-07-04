fc0c7fe2b070c9d84faba52f46afe57f73cd84e1
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-07-04 23:17:24 +0530

    Time: 359 ms (11.34%), Space: 17.7 MB (8.82%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,43 @@
+class Solution:
+    def candy(self, r: List[int]) -> int:
+        dp = [1 for i in range(len(r))]
+        topcheck = False
+        top = 0
+        res = 0
+        for i in range(1,len(r)):
+            if i-2>=0 and r[i]<r[i-1] and r[i-1]>r[i-2]:
+                topcheck = True
+                top = dp[i-1]
+                print('top',top)
+                dp[i] = 1
+            elif i-2>=0 and r[i]>r[i-1] and r[i-1]<r[i-2]:
+                if topcheck:
+                    if dp[i-1]>=top:
+                        res+=dp[i-1]+1-top
+                        top = 0
+                        topcheck=False
+                        print(res)
+                    top = 0
+                    topcheck=False
+                dp[i] = 2
+            elif r[i]==r[i-1]:
+                if topcheck:
+                    if dp[i-1]>=top:
+                        res+=dp[i-1]+1-top
+                        top = 0
+                        topcheck=False
+                top = 0
+                topcheck = False
+            elif r[i]<r[i-1] or r[i]>r[i-1]:
+                dp[i]=dp[i-1]+1
+        print(res,topcheck)
+        if topcheck:
+            print('ooooooooooooook')
+            if dp[len(r)-1]>=top:
+                res+=dp[len(r)-1]+1-top
+                top = 0
+                topcheck=False
+                print(*dp, res)
+                return sum(dp)+res 
+        print(*dp, res)
+        return sum(dp)+res

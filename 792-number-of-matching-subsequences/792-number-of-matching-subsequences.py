59056ab205b0b79a6f850524e057993154b6b17d
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-07-20 10:50:31 +0530

    Time: 1622 ms (21.75%), Space: 17.4 MB (10.57%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,39 +1,42 @@
 class Solution:
-	def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
 
-		d = {}
+        d = {}
 
-		for index in range(len(s)):
+        for index in range(len(s)):
 
-			if s[index] not in d:
-				d[s[index]] = [index]
-			else:
-				d[s[index]].append(index)
+            if s[index] not in d:
+                d[s[index]] = [index]
+            else:
+                d[s[index]].append(index)
 
-		result = []
+        result = []
 
-		for word in words:
+        for word in words:
 
-			check = 0
-			current_index = -1
+            check = 0
+            current_index = -1
 
-			for char in word:
+            for char in word:
 
-				if char not in d:
-					break
-				else:
+                if char not in d:
+                    break
 
-					for index in d[char]:
-
-						if index > current_index:
-
-							current_index = index
-							check = check + 1
-
-							break
-
-			if check == len(word):
-				result.append(word)
-
-		return len(result)  
+                elif current_index > d[char][-1]:
+                    break
+    
+                else:
         
+                    for index in d[char]:
+            
+                        if index > current_index:
+                            current_index = index
+                            check = check + 1
+            
+                            break
+
+
+                    if check == len(word):
+                        result.append(word)
+
+        return len(result)  

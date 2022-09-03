4dc3d38042cfd25526215785a42a6057cb03b690
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-09-03 10:41:05 +0530

    Time: 55 ms (73.81%), Space: 14.4 MB (21.77%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -7,18 +7,21 @@ class Solution:
                 res.append(s)
                 # print(s)
                 return
-
-            if i + k < 10:
+            
+            if i+k == i-k:
                 helper(i + k, k, n - 1, s + str(i + k))
-            if i - k >= 0:
-                helper(i - k, k, n - 1, s + str(i - k))
+            else:
+                if i + k < 10:
+                    helper(i + k, k, n - 1, s + str(i + k))
+                if i - k >= 0:
+                    helper(i - k, k, n - 1, s + str(i - k))
 
             return
 
         for i in range(1, 10):
             helper(i, k, n, str(i))
 
-        return list(set(res))
+        return res
             
             
                 

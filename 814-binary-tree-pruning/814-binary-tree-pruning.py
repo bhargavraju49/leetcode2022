1c27bc56218866db3d4f8a9ecbc537d2f85b14c3
bhargava Raju <58097588+bhargavraju49@users.noreply.github.com>
2022-09-06 09:59:36 +0530

    Time: 73 ms (5.22%), Space: 13.8 MB (68.92%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -6,33 +6,12 @@
 #         self.right = right
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def helper(root):
-            if not root:
-                return True
-            
-            l = helper(root.left)
-            r = helper(root.right)
-            # print(root.val,l,r)
-            if not root.left and not root.right:
-                if l and r and root.val == 0:
-                    return True
-                else:
-                    return False
-            else:
-                if l:
-                    root.left = None
-                if r:
-                    root.right = None
-                if l and r and root.val == 0:
-                    return True
-                else:
-                    return False
-                 
-        
-        k = helper(root)
-        if not k:
-            return root
-        else:
+        if(root==None):
             return None
         
+        root.left, root.right = self.pruneTree(root.left), self.pruneTree(root.right)
         
+        if(root.left==None and root.right==None and root.val!=1):
+            return None
+        else:
+            return root
